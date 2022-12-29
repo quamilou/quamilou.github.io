872c608a5e3965d1ec7bcc6cb6e1a00fd1e18e56
quamilou <50031737+quamilou@users.noreply.github.com>
2022-12-29 16:30:07 +0100

    site v1.0

---
+++ script.js
@@ -0,0 +1,15 @@
+let darkMode = false;
+
+function changeDarkMode(){
+    if (darkMode){
+        darkMode = false;
+        document.documentElement.style.setProperty("--text-color","black");
+        document.documentElement.style.setProperty("--background-color","#d8f1de");
+        document.getElementById("dark-light-mode").innerHTML = "Mode Sombre";
+    }else{
+        darkMode = true;
+        document.documentElement.style.setProperty("--text-color","white");
+        document.documentElement.style.setProperty("--background-color","#2b362e");
+        document.getElementById("dark-light-mode").innerHTML = "Mode Claire";
+    }
+}
